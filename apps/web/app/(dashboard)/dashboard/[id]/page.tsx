5cc140f9f54ee5665a9fcbaae3c69ec32f4f3a3b
Jona <llamasjonat@gmail.com>
2025-02-11 22:20:11 -0300

    feat(app/web): add sales limit notification and upgrade button in dashboard

--- apps/web/app/(dashboard)/dashboard/[id]/page.tsx
+++ apps/web/app/(dashboard)/dashboard/[id]/page.tsx
@@ -5,9 +5,13 @@ import { BadgeDollarSign, Contact, LoaderCircle, ReceiptText } from 'lucide-reac
 
 import { Card, CardContent, CardHeader, CardTitle } from '@workspace/ui/components/card';
 import { Satoshi } from '@workspace/ui/components/icons/satoshi';
+import { Progress } from '@workspace/ui/components/progress';
+import { Button } from '@workspace/ui/components/button';
+import { Badge } from '@workspace/ui/components/badge';
 
 import { formatBigNumbers } from '@/lib/number';
 import { db } from '@/lib/database';
+import { LIMIT_SALES_FREE } from '@/config/system';
 
 import { ProductSection } from '@/components/dashboard/product-section';
 import { CustomerSection } from '@/components/dashboard/customer-section';
@@ -110,6 +114,50 @@ export default function Page() {
 
   return (
     <>
+      <div className='relative overflow-hidden flex flex-col gap-4 w-full p-8 bg-foreground text-background rounded-lg'>
+        <div className='flex flex-col md:flex-row justify-between gap-4 md:gap-8'>
+          <div className='relative z-10 flex flex-col gap-2'>
+            <h2 className='text-lg font-semibold'>Total Sales</h2>
+            <p className='text-sm text-muted-foreground'>
+              You are using the free plan, which allows you to make up to 50 sales.
+            </p>
+          </div>
+          {/* {orderPaids?.length < LIMIT_SALES_FREE && (
+            <Button variant='secondary' size='sm' asChild>
+              <Link
+                href={`http://localhost:3000/checkout/1662cb1a7cbe920d9124b5afa3356163af6d1def89a2bec6b86fdd2cb805fa9d?email=${user?.email}`}
+              >
+                Upgrade
+              </Link>
+            </Button>
+          )} */}
+        </div>
+        <div className='relative w-full h-full'>
+          {orderPaids?.length === LIMIT_SALES_FREE && (
+            <div className='absolute z-10 top-0 left-0 flex justify-center items-center w-full h-full'>
+              <Button className='w-full' variant='secondary' size='lg' disabled={true}>
+                Upgrade <Badge>Soon</Badge>
+              </Button>
+            </div>
+          )}
+          <div
+            className={`relative flex flex-col gap-2 ${orderPaids?.length === LIMIT_SALES_FREE && 'select-none blur-[32px]'}`}
+          >
+            <Progress value={(orderPaids?.length * 100) / LIMIT_SALES_FREE} />
+            <div className='flex justify-between w-full'>
+              <div>
+                <h3 className='text-sm'>Sold out</h3>
+                <p className='text-2xl font-bold'>{orderPaids?.length}</p>
+              </div>
+              <div className='text-end'>
+                <h3 className='text-sm'>Limit</h3>
+                <p className='text-2xl font-bold'>{LIMIT_SALES_FREE}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className='grid gap-4 md:grid-cols-2'>
         <Card>
           <CardHeader className='flex flex-row items-center justify-between pb-2'>
