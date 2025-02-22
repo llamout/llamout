a2ce693c8807fad6e7617a754254604275ed6cdd
Jona <llamasjonat@gmail.com>
2025-02-22 03:50:37 -0300

    fix: general changes

--- apps/web/app/(checkout)/thank/page.tsx
+++ apps/web/app/(checkout)/thank/page.tsx
@@ -8,24 +8,24 @@ import { Suspense } from 'react';
 
 export default function Page() {
   return (
-    <Suspense>
-      <div className='flex justify-center items-center w-full h-dvh pt-12'>
-        <div className='flex flex-col items-center gap-4 w-full max-w-sm text-center px-4'>
-          <div className='flex justify-center items-center w-12 h-12 bg-gradient-to-t from-background to-white border rounded-lg shadow-sm text-muted-foreground'>
-            <PartyPopper className='size-6' />
+    <div className='flex justify-center items-center w-full h-dvh pt-12'>
+      <div className='flex flex-col items-center gap-4 w-full max-w-sm text-center px-4'>
+        <div className='flex justify-center items-center w-12 h-12 bg-gradient-to-t from-background to-white border rounded-lg shadow-sm text-muted-foreground'>
+          <PartyPopper className='size-6' />
+        </div>
+        <div className='flex flex-col gap-8 w-full'>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-2xl font-semibold'>Congratulation!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, ut?</p>
           </div>
-          <div className='flex flex-col gap-8 w-full'>
-            <div className='flex flex-col gap-2'>
-              <h1 className='text-2xl font-semibold'>Congratulation!</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, ut?</p>
-            </div>
+          <Suspense>
             <Feedback />
-          </div>
-          {/* <Button className='w-full' variant='link'>
+          </Suspense>
+        </div>
+        {/* <Button className='w-full' variant='link'>
           Go to Dashboard
         </Button> */}
-        </div>
       </div>
-    </Suspense>
+    </div>
   );
 }
