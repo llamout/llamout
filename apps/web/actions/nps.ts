0e0a645e3a5ffaa27229cc2919920919fa390037
Jona <llamasjonat@gmail.com>
2025-02-22 03:08:23 -0300

    feat(web/actions): update addNps function to use order_hash and validate feedback submission

--- apps/web/actions/nps.ts
+++ apps/web/actions/nps.ts
@@ -6,27 +6,45 @@ import { db } from '@/config/instantdb';
 import { getOrder } from './order';
 
 interface AddNps {
-  order_id: string;
+  order_hash: string;
   description: string;
   value: number;
 }
 
 export async function addNps(props: AddNps): Promise<{ error: any; data: any; ok?: boolean }> {
-  const { order_id = null, description, value } = props;
+  const { order_hash = null, description, value } = props;
 
   // TO-DO
-  if (!order_id || order_id === '') {
-    return { error: 'Product ID required', data: null };
+  if (!order_hash || order_hash === '') {
+    return { error: 'Order required', data: null };
   }
 
   // if(value < 0 || value === 0 || value > 5){
   //   return
   // }
 
+  const query = {
+    nps: {
+      $: {
+        where: {
+          order_hash,
+        },
+      },
+    },
+  };
+
   const newId = id();
 
   try {
-    const { data } = await getOrder(order_id);
+    // Validate if exist nps with order_hash
+    const { nps } = await db.query(query);
+
+    if (nps && nps?.length > 0) {
+      return { error: 'We have already recorded your feedback.', data: null };
+    }
+
+    // Get order by hash
+    const { data } = await getOrder(order_hash);
 
     if (!data?.product_id) {
       return { error: 'Order no paid', data: null };
@@ -36,6 +54,7 @@ export async function addNps(props: AddNps): Promise<{ error: any; data: any; ok
       // @ts-ignore
       db.tx.nps[newId].update({
         product_id: data?.product_id,
+        order_hash,
 
         // Data
         description: description || null,
@@ -48,6 +67,6 @@ export async function addNps(props: AddNps): Promise<{ error: any; data: any; ok
 
     return { error: null, data: newId };
   } catch (error: any) {
-    return { error: error?.body?.message, data: null, ok: error?.status !== 400 };
+    return { error: error?.body?.message, data: null };
   }
 }
