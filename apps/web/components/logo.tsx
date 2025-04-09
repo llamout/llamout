4c599fdc14879af910c9e5c8a674db54c2364e26
Jona <llamasjonat@gmail.com>
2025-04-09 16:24:32 -0300

    feat(web): added Logo component

---
+++ apps/web/components/logo.tsx
@@ -0,0 +1,8 @@
+export function Logo(props: any) {
+  return (
+    <svg {...props} width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M13.9183 25H12V7H13.9183V25Z' fill='currentColor' />
+      <path d='M20 25H18.0817V7H20V25Z' fill='currentColor' />
+    </svg>
+  );
+}
